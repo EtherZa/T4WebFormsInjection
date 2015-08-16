b6c48e68860a0e6ba7585027080465452ce4c994
Richard Pringle <richard.pringle@gmail.com>
2015-08-16 22:37:29 +0200

    Add: Include master pages Fix: typo

---
+++ src/T4WebFormsInjection.App/MasterPages/Site1.Master.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.UI;
+using T4WebFormsInjection.App.Code;
+
+namespace T4WebFormsInjection.App.MasterPages
+{
+    public partial class Site1 : MasterPage
+    {
+        public Site1(IDummyInterface dummyInterface)
+        {
+        }
+    }
+}
