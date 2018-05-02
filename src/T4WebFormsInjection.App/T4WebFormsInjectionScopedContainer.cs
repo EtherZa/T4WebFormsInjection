94f9d550cc216390b7514d0cb8c3bb50d5783d9c
Richard Pringle <richard.pringle@riotinto.com>
2018-05-02 11:48:54 +0800

    Deprecated specifying methods to resolve and release instances in favour of specifying an interface with the a concrete class specified in the settings. This was done in order to allow for IOC frameworks which make use of scopes for a unit of work eg. Autofac

---
+++ src/T4WebFormsInjection.App/T4WebFormsInjectionScopedContainer.cs
@@ -0,0 +1,51 @@
+﻿namespace T4WFI.App
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Sample container implementation for T4WebformsInjection for IOC framework that does not make use of scoped lifetimes.
+    /// </summary>
+    /// <remarks>
+    /// This sample needs to be customized to make use of your IOC implementation.
+    /// </remarks>
+    internal class T4WebFormsInjectionScopedContainer : T4WebFormsInjection.IScopedContainer
+    {
+        private readonly List<object> trackedObjects;
+
+        /// <summary>
+        /// Constructs a new instance of the <see cref="T4WebFormsInjectionScopedContainer"/> class.
+        /// </summary>
+        public T4WebFormsInjectionScopedContainer()
+        {
+            this.trackedObjects = new List<object>();
+        }
+
+        /// <summary>
+        /// Resolve instance of <see cref="T"/>.
+        /// </summary>
+        /// <typeparam name="T">Type to be resolved.</typeparam>
+        /// <returns>Instance of <see cref="T"/></returns>
+        public T Resolve<T>()
+            where T : class
+        {
+            // TODO: Customize for IOC implementation
+            var o = Code.Container.GetInstance<T>();
+            this.trackedObjects.Add(o);
+            return o;
+        }
+
+        /// <summary>
+        /// Release all instances created by this scoped container.
+        /// </summary>
+        public void ReleaseAll()
+        {
+            foreach (var trackedObject in this.trackedObjects)
+            {
+                // TODO: Customize for IOC implementation
+                Code.Container.Release(trackedObject);
+            }
+
+            this.trackedObjects.Clear();
+        }
+    }
+}
---
+++ src/T4WebFormsInjection.App/Template/T4WebFormsInjection.DependencyTracker.cs.pp
@@ -0,0 +1,49 @@
+﻿namespace $rootnamespace$
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Sample container implementation for T4WebformsInjection for IOC framework that does not make use of scoped lifetimes.
+    /// </summary>
+    /// <remarks>
+    /// This sample needs to be customized to make use of your IOC implementation.
+    /// </remarks>
+    internal class T4WebFormsInjectionScopedContainer : $rootnamespace$.T4WebFormsInjection.IScopedContainer
+    {
+        private readonly List<object> trackedObjects;
+
+        /// <summary>
+        /// Constructs a new instance of the <see cref="T4WebFormsInjectionScopedContainer"/> class.
+        /// </summary>
+	    public T4WebFormsInjectionScopedContainer()
+	    {
+	        this.trackedObjects = new List<object>();
+	    }
+
+		/// <summary>
+		/// Resolve instance of <see cref="T"/>.
+		/// </summary>
+		/// <typeparam name="T">Type to be resolved.</typeparam>
+		/// <returns>Instance of <see cref="T"/></returns>
+		public T Resolve<T>()
+			where T: class
+		{
+			// TODO: Resolve instance here
+			this.trackedObjects.Add(o);
+			return o;
+		}
+
+		/// <summary>
+		/// Release all instances created by this scoped container.
+		/// </summary>
+	    public void ReleaseAll()
+	    {
+	        foreach (var trackedObject in this.trackedObjects)
+	        {
+				// TODO: release instance
+	        }
+
+			this.trackedObjects.Clear();
+	    }
+    }
+}
